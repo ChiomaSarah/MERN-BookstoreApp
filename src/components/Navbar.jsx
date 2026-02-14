a88f85f32b16ee7a83fdaff70a21e13b5dd9633f
CHIOMA SARAH OSUJI <78868674+ChiomaSarah@users.noreply.github.com>
2026-02-15 00:33:37 +0100

    Update Navbar.jsx: close mobile menu on item click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,79 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import useToken from "../useToken";
 
 function Navbar() {
   const { token } = useToken();
+  const [isOpen, setIsOpen] = useState(false);
 
-  // let buttons;
-
-  // if (token) {
-  //   buttons = (
-  //     <>
-  //       <h6 style={{ paddingTop: "12px" }}>
-  //         <Link
-  //           to="/auth/logout"
-  //           className="link"
-  //           onClick={() => window.location.reload()}
-  //         >
-  //           Logout
-  //         </Link>
-  //       </h6>
-  //     </>
-  //   );
-  // } else {
-  //   buttons = (
-  //     <>
-  //       <h6 style={{ paddingTop: "12px" }}>
-  //         <Link to="/auth/login" className="link">
-  //           Login
-  //         </Link>
-  //       </h6>
-  //       <h6 style={{ paddingTop: "12px" }}>
-  //         <Link to="/auth/signup" className="link">
-  //           Signup
-  //         </Link>
-  //       </h6>
-  //     </>
-  //   );
-  // }
-  // return (
-  //   <nav className="navbar bg-info">
-  //     <h4 id="logo-font">BOOK STORE</h4>
-  //     <h6 style={{ paddingTop: "12px" }}>
-  //       <Link to="/" className="link">
-  //         Home
-  //       </Link>
-  //     </h6>
-  //     {buttons}
-  //   </nav>
-  // );
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+  
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-      <Link to="/">
+      <Link to="/" onClick={closeMenu}>
         <h3 className="text-light navbar-brand">BOOK STORE</h3>
       </Link>
 
       <button
         className="navbar-toggler bg-dark"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarTogglerDemo02"
-        aria-controls="navbarTogglerDemo02"
-        aria-expanded="false"
+        onClick={toggleMenu}
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
           <li className="nav-item">
             <Link
               to="/"
               className="nav-link"
-              id="navbar-list"
+              onClick={closeMenu}
               style={{ fontSize: "18px" }}
             >
               Home
@@ -85,7 +43,7 @@ function Navbar() {
               <Link
                 to="/books"
                 className="nav-link"
-                id="navbar-list"
+                onClick={closeMenu}
                 style={{ fontSize: "18px" }}
               >
                 Books
@@ -96,7 +54,7 @@ function Navbar() {
               <Link
                 to="/auth/login"
                 className="nav-link"
-                id="navbar-list"
+                onClick={closeMenu}
                 style={{ fontSize: "18px" }}
               >
                 Login
@@ -104,25 +62,28 @@ function Navbar() {
             </li>
           )}
 
-          {token ? null : (
+          {!token && (
             <li className="nav-item">
               <Link
                 to="/auth/signup"
                 className="nav-link"
-                id="navbar-list"
+                onClick={closeMenu}
                 style={{ fontSize: "18px" }}
               >
                 SignUp
               </Link>
             </li>
           )}
-          {!token ? null : (
+
+          {token && (
             <li className="nav-item">
               <Link
                 to="/auth/logout"
                 className="nav-link"
-                onClick={() => window.location.reload()}
-                id="navbar-list"
+                onClick={() => {
+                  closeMenu();
+                  window.location.reload();
+                }}
                 style={{ fontSize: "18px" }}
               >
                 Logout
@@ -134,4 +95,5 @@ function Navbar() {
     </nav>
   );
 }
+
 export default Navbar;
